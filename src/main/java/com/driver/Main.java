6a434006897c43307798e2140a5412d00963b076
Radhika104 <87032257+Radhika104@users.noreply.github.com>
2023-05-20 09:55:58 +0530

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,19 @@
 package com.driver;
-class A
+public class Main {
+public static class A
 {
 public String meth()
 {
 return "Invoking method from class A";
 }
 }
-class B extends A
+public static class B extends A
 {
 public String meth()
 {
 return "Invoking method from class B";
 }
 }
-public class Main {
 public static void main(String args[])
 {  
 B obj=new B();
